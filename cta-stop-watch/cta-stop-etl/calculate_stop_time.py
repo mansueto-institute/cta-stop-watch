a18e171e7cbaed8206cdb891d086b48b4579d8b8
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-06-05 14:03:42 -0500

    docstrings and cleaner map

--- cta-stop-watch/cta-stop-etl/calculate_stop_time.py
+++ cta-stop-watch/cta-stop-etl/calculate_stop_time.py
@@ -4,15 +4,15 @@ import geopandas as gpd
 from geopandas import GeoDataFrame
 import sys
 from shapely import wkt
+import re
 
 ## CONSTANTS
 M_TO_FT = 3.280839895
-BUFFER_DIST = 50.0
 
 
 def prepare_segment(pid: str):
     """
-    prepares the segments for a pattern
+    prepares the created segments from a pattern (pid) for use with the bus location. 
     """
     # load segment
     segments_df = pd.read_parquet(
@@ -30,7 +30,7 @@ def prepare_segment(pid: str):
 
 def prepare_trips(pid: str):
     """
-    prepare the real trips for pattern
+    prepare the real trips from a pattern (pid) for use with the segments
     """
 
     # load trips for a pattern
@@ -51,6 +51,9 @@ def prepare_trips(pid: str):
 
 
 def prepare_stops(pid: str):
+    """
+    Prepares the stops from a pattern (pid) for use with the bus location
+    """
 
     stops_df = pd.read_parquet(
         f"cta-stop-watch/cta-stop-etl/out/patterns/pid_{pid}_stop.parquet"
@@ -65,25 +68,9 @@ def prepare_stops(pid: str):
     return stops_df
 
 
-def process_one_trip(trip_id:str,
-    trip_df: GeoDataFrame, segments_df: GeoDataFrame, stops_df: GeoDataFrame
-):
-    """
-    for one trip, merge the real pings with the segments
-    """
-
-    df = merge_segments_trip(trip_df, segments_df, stops_df)
-    df['unique_trip_vehicle_day'] = trip_id
-
-    # TODO
-    # df = interpolate_stoptime(df)
-
-    return df
-
-
 def merge_segments_trip(trip_gdf, segments_gdf, stops_df):
     """
-    create full trip data frame from bus locations and segment stops
+    Confirm bus locations are on route and then create route df with bus location
     """
 
     # merge the bus locations with the segments to find the segment that the bus is in
@@ -112,21 +99,43 @@ def merge_segments_trip(trip_gdf, segments_gdf, stops_df):
 
     return final_df
 
-
 def interpolate_stoptime(trip_df, bus_location_index: str):
-
+    """
+    given a route df with stops and bus location, interpolate the time when the bus is at each stop
+    """
     # TODO
     pass
+
+def process_one_trip(trip_id:str,
+    trip_df: GeoDataFrame, segments_df: GeoDataFrame, stops_df: GeoDataFrame
+):
+    """
+    process one trip to return a df with the time a bus is at each stop.
+    for one trip, only keep points that are on route, then create route df 
+    with bus location, then interpolate time when bus is at each stop. 
+    """
+
+    df = merge_segments_trip(trip_df, segments_df, stops_df)
+    df['unique_trip_vehicle_day'] = trip_id
+
+    # TODO
+    # df = interpolate_stoptime(df)
+
+    return df
+
 
 
 def process_pattern(pid: str, tester:int = float("inf")):
     """
-    take real time bus data and merge with the segments dataframe.
-    each row will be a bus location and the segment that it is in.
+    Process all the trips for one pattern to return a df with the time a bus is at each stop for every trip.
     """
-    # is pattern available from the api?
-    # TODO
-
+    # is data for this pattern available?
+    try:
+        prepare_segment(pid)
+    except NameError:
+        print(f"Pattern {pid} not available")
+        return False
+    
     # prepare the segments
     segments_gdf = prepare_segment(pid)
 
@@ -144,6 +153,8 @@ def process_pattern(pid: str, tester:int = float("inf")):
         processed_trip_df = process_one_trip(trip_id,trip_gdf, segments_gdf, stops_df)
 
         all_trips_gdf = pd.concat([all_trips_gdf, processed_trip_df], axis=0).reset_index(drop=True)
+
+        # for testing
         count += 1
         if count >= int(tester):
             break
@@ -152,41 +163,34 @@ def process_pattern(pid: str, tester:int = float("inf")):
 
 def process_all_patterns():
     """
-    run everything
-
+    Process all the trips for all patterns available. Export one file per pattern.
     """
-
-    # TODO 
-    """
-    PID_DIR = "out/pids"
+    
+    PID_DIR = "out/pids/patterns"
     for pid_file in os.listdir(PID_DIR):
-        # print(f"{PID_DIR}/{pid_file}")
-        pid_df = pd.read_parquet(f"{PID_DIR}/{pid_file}")
-        pid_df.loc[:, "tmstmp"] = pd.to_datetime(
-            pid_df.loc[:, "tmstmp"], format="%Y%m%d %H:%M"
-        )
-        pid_df = gpd.GeoDataFrame(
-            pid_df,
-            geometry=gpd.GeoSeries.from_xy(
-                x=pid_df.loc[:, "lon"], y=pid_df.loc[:, "lat"], crs="EPSG:4326"
-            ),
-        )
-        pid = pid_file.replace(".0.parquet", "")
-        print(pid_df)
-        if process_pattern(pid, pid_df):
-            break
-        """
+    
+        numbers = re.findall('\d+', pid_file)
+        pid = numbers[0]
+        print(pid)
+
+        result = process_pattern(pid)
+
+        # do something with the result
+        #result.to_csv(f'out/full_trips/pid_{pid}_all_trips.csv', index=False)
 
 if __name__ == "__main__":
     """
    adding in a test for one pattern
     """
-    if sys.argv[1] == 'process_pattern' and len(sys.argv) != 3:
+    if sys.argv[1] == 'process_pattern' and len(sys.argv) > 3:
         print("Usage: python -m process_pattern <pid> <[optional] number of trips to process>")
         sys.exit(1)
     elif sys.argv[1] == 'process_pattern':
-        result = process_pattern(sys.argv[2], sys.argv[3])
-        print(result.shape)
+        if sys.argv[3]:
+            result = process_pattern(sys.argv[2], sys.argv[3])
+        else:
+            result = process_pattern(sys.argv[2])
+
         result.to_csv('test_full_pattern.csv', index=False)
     
     if sys.argv[1] == 'process_all_patterns':
