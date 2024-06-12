71133a8aa8491f1537194b424b65b0f8e0555f03
Regina I. Medina <rmedina@uchicago.edu>
2024-06-12 10:36:56 -0500

    Fixing typos

--- cta-stop-watch/acs/get_sociodems.R
+++ cta-stop-watch/acs/get_sociodems.R
@@ -1,13 +1,9 @@
 # 0. Imports -------------------------------------------------------------------
 
-install.packages("sfarrow")
 
 # Load libraries
-library(tidycensus)
-library(tidyverse)
-library(arrow)
-library(sf)
-library(sfarrow)
+library(pacman)
+p_load(tidycensus, tidyverse, arrow, sf, sfarrow, mapview, tigris)
 
 # Load census API key from environment
 readRenviron("~/.Renviron")
@@ -41,7 +37,8 @@ df_tot_pop <- get_acs(
     geography = "tract", 
     state = "IL",
     variables = c(
-        total_pop = "B01001_003"
+        total_pop = "B02001_001", 
+        medincome = "B19013_001"
     ),
     year = 2020, 
     geometry = TRUE, 
@@ -70,5 +67,60 @@ st_crs(df_tot_pop) = "EPSG:4326"
 sf::st_join(df_tot_pop, df_stops)
 
 sf::st_within(df_tot_pop, df_stops)
+
+
+
+# 3. Tracts and stops ----------------------------------------------------------
+
+sf_il_tracts <- tigris::tracts(state='17', year=2021, cb=TRUE)
+
+sf_il_tracts
+
+sf_cook_tracts_acs <- sf_il_tracts %>% 
+    left_join(
+        df_tot_pop %>% 
+            select(GEOID, contains('Share'), total_pop, medincome)
+    ) %>% 
+    filter(COUNTYFP == '031') #cook
+
+
+# Maps -------------------------------------------------------------------------
+
+il21 <- get_acs(geography = "tract", 
+                variables = c(medincome = "B19013_001",
+                              totalpop = "B02001_001",
+                              white_alone = "B02001_002",
+                              black_alone = "B02001_003",
+                              asian_alone = "B02001_004"),
+                state = "IL", 
+                year = 2021,
+                output='wide')
+
+il21 <- il21 %>% 
+    mutate(
+    `White Share` = white_aloneE / totalpopE,
+    `Black Share` = black_aloneE / totalpopE,
+    `Other Share` = (totalpopE - white_aloneE - black_aloneE) / totalpopE
+)
+
+iltracts <- tigris::tracts(state='17', year=2021, cb=TRUE)
+
+
+cook_tracts_acs <- iltracts %>% 
+    left_join(
+    il21 %>% 
+        select(GEOID, contains('Share'), totalpopE, medincomeE)
+    ) %>% 
+    filter(COUNTYFP == '031') #cook
+
+ilcountysub <- tigris::county_subdivisions(state=17, county=31, cb=TRUE)
+chicago <- ilcountysub %>% filter(NAME == "Chicago") %>% select(region=NAME)
+
+chicago_tracts_acs <- cook_tracts_acs %>% st_intersection(
+    chicago
+)
+
+mapview(chicago) + 
+    mapview(chicago_tracts_acs, alpha=.05, col.regions='blue')
 
 # END. -------------------------------------------------------------------------
--- cta-stop-watch/acs/merge_communities_stops.py
+++ cta-stop-watch/acs/merge_communities_stops.py
@@ -1,6 +1,6 @@
 import os
 import pathlib
-import geopandas as gdp
+import geopandas as gpd
 import logging
 
 # Configure a log file to be able to read the logging messages 
@@ -13,13 +13,11 @@ logging.basicConfig(filename='spatial_join.log',
 
 
 # Define functions 
-def merge_communities_stops(gdf_areas: gdp.GeoDataFrame, gdf_stops: gdp.GeoDataFrame) -> gdp.GeoDataFrame: 
-
-    # 
+def merge_communities_stops(gdf_areas: gpd.GeoDataFrame, gdf_stops: gpd.GeoDataFrame) -> gpd.GeoDataFrame: 
     
     # Check projection for Chicago
-    gdf_areas = gdp.GeoDataFrame(gdf_areas, crs = "EPSG:4326")
-    gdf_stops = gdp.GeoDataFrame(gdf_stops, crs = "EPSG:4326")
+    gdf_areas = gpd.GeoDataFrame(gdf_areas, crs = "EPSG:4326")
+    gdf_stops = gpd.GeoDataFrame(gdf_stops, crs = "EPSG:4326")
     gdf_stops["geoms_stops"] = gdf_stops["geometry"]
 
     # logging.info(f"Column names from AREAS {gdf_areas.columns = }")
@@ -41,11 +39,11 @@ if __name__ == "__main__":
     SHAPEFILES_DIR = str(pathlib.Path(__file__).parent.parent / "shapefiles/")
 
     # Load shapefile 
-    gdf_communities = gdp.read_file(
+    gdf_communities = gpd.read_file(
         SHAPEFILES_DIR + "/Boundaries - Community Areas (current).geojson")
 
     # Load bustops 
-    gdf_stops = gdp.read_file(
+    gdf_stops = gpd.read_file(
         SHAPEFILES_DIR + "/CTA_BusStops/CTA_BusStops.shp")
 
     # Spatial join to merge data
