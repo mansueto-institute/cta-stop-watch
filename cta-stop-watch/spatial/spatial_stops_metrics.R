ec659c65c723dfce45ac598a7c010d817328a5bc
Regina I. Medina <rmedina@uchicago.edu>
2024-06-24 20:21:43 -0500

    Stop's land coverage

--- cta-stop-watch/spatial/spatial_stops_metrics.R
+++ cta-stop-watch/spatial/spatial_stops_metrics.R
@@ -1,4 +1,4 @@
-# 0. Imports -------------------------------------------------------------------
+# 0. SET UP --------------------------------------------------------------------
 
 # setwd("\\\\wsl.localhost/Ubuntu/home/rmedina/mansueto/cta-stop-watch/cta-stop-watch/acs")
 
@@ -7,24 +7,31 @@ rm(list = ls())
 
 # Load libraries
 library(pacman)
-p_load(tidycensus, tidyverse, arrow, sf, sfarrow, mapview, tigris)
+p_load(tidycensus, tidyverse, arrow, sf, sfarrow, mapview, tigris, tmap)
 
 # Load census API key from environment
 readRenviron("~/.Renviron")
 Sys.getenv("CENSUS_API_KEY")
 
-# 1. Load Shapefiles -----------------------------------------------------------
+# Constans
+MILE_IN_METERS  <- 1609.34
+CHICAGO_BLOCK   <- MILE_IN_METERS / 8
+
+# 1. LOAD SHAPEFILES------------------------------------------------------------
 
 ## 1.1. Chicago Data Portal ----------------------------------------------------
 
-### 1.1.1. Bus stops -----------------------------------------------------------
+### Bus stops ------------------------------------------------------------------
 
 # Load community areas shapefile with stops info
 sf_stops <- sf::st_read("../shapefiles/CTA_BusStops/CTA_BusStops.shp")
 
-### 1.1.2. Community areas (77) ------------------------------------------------
+### Community areas (77) -------------------------------------------------------
 
 sf_communities <- sf::st_read("../shapefiles/Boundaries - Community Areas (current).geojson")
+
+# Compute area in meters
+sf_communities$area_m2 <- st_area(sf_communities)
 
 ## 1.2. Census tracts ----------------------------------------------------------
 
@@ -45,7 +52,7 @@ sf_cook_tracts <- sf_il_tracts |> filter(COUNTYFP == '031')
 sf_chicago_tracts <- sf_cook_tracts |> st_intersection(sf_chicago)
 
 
-# 1.3. Standardize projection --------------------------------------------------
+## 1.3. Standardize projection -------------------------------------------------
 
 sf_stops            <- st_transform(sf_stops      , crs = "EPSG:4326")
 sf_communities      <- st_transform(sf_communities, crs = "EPSG:4326")
@@ -58,14 +65,14 @@ sf_cook_tracts      <- sf_cook_tracts |> st_set_crs("EPSG:4326")
 sf_chicago_tracts   <- sf_chicago_tracts |> st_set_crs("EPSG:4326")
 
 
-# 2. Spatial join for stops ----------------------------------------------------
+# 2. POINT-IN-POLYGON BUS COUNT ------------------------------------------------
 
-## STOPS BY CENSUS TRACT -------------------------------------------------------
+## 2.1. Census Tract -----------------------------------------------------------
 
-# Join stops
-sf_stops_tracts <- sf::st_join(sf_cook_tracts, sf_stops, 
-                               join = st_intersects, 
-                               left = TRUE)
+# st_crs(sf_cook_tracts)$units
+# 
+# sf_cook_tracts_buffer <- st_buffer(sf_cook_tracts, 1)
+# mapview(sf_cook_tracts)
 
 # Count stops by Census Tract 
 sf_cook_tracts$n_stops    <- lengths(st_intersects(sf_cook_tracts, sf_stops))
@@ -75,23 +82,158 @@ mapview(sf_chicago) +
     mapview(sf_chicago_tracts, alpha=.05, zcol = "n_stops") +
     mapview(sf_stops, alpha = 0, size = 2, cex = 1, col.regions = "grey")
 
-## STOPS BY COMMUNITY AREA -----------------------------------------------------
+## 2.2 Community area ----------------------------------------------------------
 
-df_avg_time <- arrow::read_parquet("../analysis/aggregated_times_stops.parquet")
+# Exploratory analysis of community geometries and bus stops 
 
-# Count stops by Census Tract 
-sf_communities$n_stops <- lengths(st_intersects(sf_communities, sf_stops))
-
+# Single solid color for community areas and bus stops as bright red dots
 mapview(sf_chicago) + 
     mapview(sf_communities, alpha=0.6) +
     mapview(sf_stops, alpha = 0, size = 4, cex = 1, 
             col.regions = "red")
 
-mapview(sf_chicago) + 
-    mapview(sf_communities, alpha=.05, zcol = "n_stops") +
-    mapview(sf_stops, alpha = 0, size = 2, cex = 1, 
-            col.regions = "grey")
+# Create buffers around community areas to include bus stops in the border
+sf_communities_buffer <- st_buffer(sf_communities, dist = CHICAGO_BLOCK)
 
+mapview(sf_communities, col.regions = "red") +
+    mapview(sf_communities_buffer)
+
+mapview(sf_communities_buffer) +
+    mapview(sf_communities, col.regions = "red")
+
+# Count stops by Census Tract 
+sf_communities$n_stops_strict <- lengths(st_intersects(sf_communities, sf_stops))
+sf_communities$n_stops_buffer <- lengths(st_intersects(sf_communities_buffer, sf_stops))
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "n_stops_strict")
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "n_stops_buffer") 
+
+
+# 3. BUS STOP WITHIN WALKING DISTANCE COVERAGE ---------------------------------
+
+# Code trial for defining function 
+
+# TODO: 
+    # Check projections are the same
+    # Check units of projections
+    # Check units of union buffer
+
+
+# # What is a decent walking distance?
+# sf_stops_buffers    <- st_buffer(sf_stops, CHICAGO_BLOCK)
+# sf_stops_union      <- st_union(sf_stops_buffers)
+# sf_stops_coverage   <- sf_stops_union[2]
+
+# Sanity checks for buffers
+# sf::st_is_valid(sf_stops_coverage)
+# 
+# # Correct mistakes from buffer
+# sf_stops_coverage <- sf::st_make_valid(sf_stops_coverage)
+# sf::st_is_valid(sf_stops_coverage)
+# 
+# 
+# # mapview(sf_stops_buffers, alpa = 0.2) + 
+# #     mapview(sf_stops, col.regions = "red") 
+# 
+# # Visualize union of buffers 
+# tm_shape(sf_communities) + 
+#     tm_borders() +
+#     tm_shape(sf_stops_coverage) + 
+#     tm_fill(col = "blue", alpha = .4) + 
+#     tmap_options(check.and.fix = TRUE) +
+#     tm_borders(col = "blue")
+# 
+# # get area coverage 
+# sf_stop_intersect <- st_intersection(sf_communities, sf_stops_coverage) 
+# 
+# sf_stop_coverage_percent <- sf_stop_intersect |> 
+#     mutate(intersect_area = st_area(geometry),
+#            intersect_area = as.numeric(intersect_area),
+#            shape_area = as.numeric(shape_area),
+#            area_coverage = intersect_area / shape_area
+#            ) |> 
+#     st_drop_geometry() |> 
+#     dplyr::select(community, contains("area"))
+# 
+# 
+# sf_communities_stops <- sf_communities |> 
+#     select(-area_coverage) |> 
+#     left_join(sf_stop_coverage_percent |> 
+#                   select(community, area_numbe, area_coverage))
+# 
+# mapview(sf_chicago) + 
+#     mapview(sf_communities_stops, alpha=.05, zcol = "area_coverage") 
+
+
+get_stops_land_coverage <- function(num_blocks = 1){
+    # Create buffers for specified number of blocks and correct if mistaken
+    sf_stops_buffers    <- st_buffer(sf_stops, CHICAGO_BLOCK * num_blocks)
+    sf_stops_coverage   <- st_union(sf_stops_buffers)
+    sf_stops_coverage <- sf::st_make_valid(sf_stops_coverage)
+    
+    if(length(sf_stops_buffers) == 2){
+        sf_stops_coverage   <- sf_stops_coverage[2]    
+    }
+    
+    # if(!sf::st_is_valid(sf_stops_coverage)){
+    #     # Correct mistakes from buffer
+    #     sf_stops_coverage <- sf::st_make_valid(sf_stops_coverage)
+    # }
+    
+    
+    # Compute are of coverage 
+    sf_stop_intersect <- st_intersection(sf_communities, sf_stops_coverage) 
+    
+    sf_stop_coverage_percent <- sf_stop_intersect |> 
+        mutate(
+            intersect_area_m2 = st_area(geometry),
+            intersect_area_m2 = as.numeric(intersect_area_m2),
+            area_m2 = as.numeric(area_m2),
+            area_coverage = intersect_area_m2 / area_m2
+        ) |> 
+        st_drop_geometry() |> 
+        dplyr::select(community, contains("area"))
+
+    
+    return(sf_stop_coverage_percent)
+    
+}
+
+df_coverage_1block <- get_stops_land_coverage(1)
+df_coverage_2block <- get_stops_land_coverage(num_blocks = 2)
+df_coverage_3block <- get_stops_land_coverage(3)
+df_coverage_4block <- get_stops_land_coverage(4)
+
+beepr::beep(4)
+
+sf_communities$percent_1b <- df_coverage_1block$area_coverage
+sf_communities$percent_2b <- df_coverage_2block$area_coverage
+sf_communities$percent_3b <- df_coverage_3block$area_coverage
+sf_communities$percent_4b <- df_coverage_4block$area_coverage
+
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "percent_1b") 
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "percent_2b") 
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "percent_3b") 
+
+mapview(sf_chicago) + 
+    mapview(sf_communities, alpha=.05, zcol = "percent_4b") 
+
+
+sf_community_stops_metrics <- sf_communities |> 
+    select(community, area_numbe, contains("n_"), contains("percent_"), geometry)
+
+
+st_write_parquet(sf_community_stops_metrics, 
+                     "sf_community_stops_metrics.parquet")
 
 # 2. ACS preprocessing ---------------------------------------------------------
 
@@ -131,7 +273,7 @@ sf_tot_pop_tract <- get_acs(
 
 # 3. MAPS ----------------------------------------------------------------------
 
-# CENSUS TRACT -----------------------------------------------------------------
+## Map Census Tract ------------------------------------------------------------
 
 # Add pop data by census tract 
 names(sf_chicago_tracts) 
@@ -161,10 +303,10 @@ mapview(sf_chicago) +
 
 
 
-# COMMUNITY --------------------------------------------------------------------
+## Map Community Area ----------------------------------------------------------
 
 
-## SPATIAL JOIN -------------
+### Spatial aggregation of pop by tracts to community --------------------------
 
 # Aggregate data at the community level 
 sf_tot_pop_tract <- st_transform(sf_tot_pop_tract, crs = "EPSG:4326")
@@ -177,7 +319,7 @@ sf_tot_pop_comms <- sf_communities %>%
 
 # Quality Checks 
 sum(sf_tot_pop_tract$pop) # 1,774,605
-sum(sf_tot_pop_comms$pop) # 1,714,806 # Just communitites in Chicago
+sum(sf_tot_pop_comms$pop) # 1,714,806 # Just communities in Chicago
 
 nrow(sf_stops) # All stops 
 sum(sf_communities$n_stops) # Stops in Chicago communities
@@ -199,7 +341,10 @@ mapview(sf_chicago) +
     mapview(sf_tot_pop_comms, alpha=.05, zcol = "stops_percap") 
 
 
-## Imputed time data -----------------------------------------------------------
+# 4. Time indicators -----------------------------------------------------------
+
+# Load time indicators
+df_avg_time <- arrow::read_parquet("../analysis/aggregated_times_stops.parquet")
 
 # Paste point geometries 
 sf_stops_routes <- df_avg_time |>
