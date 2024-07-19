4e9adb4fd27931ff52f1d5ab0c2d86f71b016f3f
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2024-07-19 16:00:02 -0500

    add shapes and toggle

--- cta-stop-watch/bus_report_cards/static/main.js
+++ cta-stop-watch/bus_report_cards/static/main.js
@@ -1,14 +1,11 @@
+
+//map.setLayoutProperty(layer, 'visibility', 'none')
 const ChiLocation = [-87.63211524853163, 41.862161325588076];
+const layers = ['stop', 'route', 'ca']
 
-dataPath = "data/bus_stops.geojson"
-
-d3.json(dataPath, function (df) {
-  sessionStorage.setItem("data", JSON.stringify(df));
-});
-
-const stops = JSON.parse(sessionStorage.getItem("data"))
-
-
+stopsPath = "data/bus_stops.geojson"
+routesPath = "data/routes.geojson"
+caPath = "data/ca_boundaries.geojson"
 
 var map = new maplibregl.Map({
     container: 'map',
@@ -21,14 +18,26 @@ var map = new maplibregl.Map({
 map.on('load', () => {
     map.addSource('stops', {
         'type': 'geojson',
-        'data': stops
+        'data': stopsPath
+    });
+
+    map.addSource('routes', {
+        'type': 'geojson',
+        'data': routesPath
+    });
+
+    map.addSource('cas', {
+        'type': 'geojson',
+        'data': caPath
     });
 
     map.addLayer({
-        'id': 'stops',
+        'id': 'stop',
         'type': 'circle',
         'source': 'stops',
-        'layout': {},
+        'layout': {
+            'visibility': 'visible'
+        },
         'paint': {
             'circle-color': '#800000',
             'circle-radius': {
@@ -41,12 +50,78 @@ map.on('load', () => {
         }
     });
 
-    map.on('mouseenter', 'stops', (e) => {
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'routes',
+        'layout': {
+            'line-cap': 'round',
+            'visibility': 'none'},
+        'paint': {
+            'line-color': 'blue',
+            'line-width': {
+                'base': 2,
+                'stops': [
+                    [12, 2],
+                    [22, 10]
+                ]
+            },
+        }
+    });
+
+    map.addLayer({
+        'id': 'ca',
+        'type': 'fill',
+        'source': 'cas',
+        'layout': {
+            'visibility': 'none'},
+        'paint': {
+            'fill-color': 'green',
+            'fill-outline-color': "white",
+            'fill-opacity': 0.5
+        }
+    });
+
+    map.on('mouseenter', 'stop', (e) => {
         // Change the cursor style as a UI indicator.
         map.getCanvas().style.cursor = 'pointer';
     
         document.getElementById('report-cards').innerHTML = e.features[0].properties.public_nam;
-        console.log(e)
- })
+        //console.log(e)
+    })
+    map.on('mouseenter', 'route', (e) => {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+    
+        document.getElementById('report-cards').innerHTML = e.features[0].properties.ROUTE;
+        //console.log(e)
+    })
+
+    map.on('mousemove', 'ca', (e) => {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+    
+        document.getElementById('report-cards').innerHTML = e.features[0].properties.community;
+    })
+
+ map.on('click', 'stop', (e) => {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+
+    document.getElementById('report-cards').innerHTML = e.features[0].properties.public_nam + "<br />" + "Services route(s) " + e.features[0].properties.routesstpg;
+})
  
 });
+
+
+
+
+function showLayer(layer) {
+
+    map.setLayoutProperty(layer, 'visibility', 'visible')
+    toRemove = layers.filter(l => l !== layer);
+
+    for (l of toRemove) {
+            map.setLayoutProperty(l, 'visibility', 'none')
+        }
+    }
