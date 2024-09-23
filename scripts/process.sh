8a0ae830cb7ef1412c40a1181dc89442d7f1482e
Divij Sinha (CTA Server) <divijs@uchicago.edu>
2024-09-23 17:00:04 +0000

    add daily combine ghostbus to process script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+cd /home/divijs_uchicago_edu/cta-stop-watch/cta-stop-watch/ghostbus-cta-scrape
+/home/divijs_uchicago_edu/cta-stop-watch/.venv/bin/python combine_daily_files.py
+
 cd /home/divijs_uchicago_edu/cta-stop-watch/cta-stop-watch/report_automation
 /home/divijs_uchicago_edu/cta-stop-watch/.venv/bin/python -m main -p process
