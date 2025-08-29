0231ffc529e80114a3fc55ca07bc2e9bfaf93ee7
Regina I Medina <rmedina@uchicago.edu>
2025-08-29 16:57:23 +0000

    Update cron job dirs

--- scripts/daily_combine.sh
+++ scripts/daily_combine.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cd /home/divijs_uchicago_edu/cta-stop-watch/cta-stop-watch/ghostbus-cta-scrape
-/home/divijs_uchicago_edu/cta-stop-watch/.venv/bin/python combine_daily_files.py | tee daily_combine.log
+cd /home/rmedina_uchicago_edu/cta-stop-watch/cta-stop-watch/ghostbus-cta-scrape
+/home/rmedina_uchicago_edu/cta-stop-watch/.venv/bin/python combine_daily_files.py | tee daily_combine.log
