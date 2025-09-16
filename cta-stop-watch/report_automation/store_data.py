b0d3638b63093584b2c62e87958cd65b24935e88
Regina I Medina <rmedina@uchicago.edu>
2025-09-16 17:48:29 -0500

    Rename bucket refs

--- cta-stop-watch/report_automation/store_data.py
+++ cta-stop-watch/report_automation/store_data.py
@@ -1,11 +1,12 @@
 # Libraries -------------------------------------------------------------------
+
 import subprocess
 from datetime import date
 
 # Contants --------------------------------------------------------------------
 
 # urls
-s3_path = "gs://miurban-dj-public/cta-stop-watch"
+BUCKET_PATH = "gs://miurban-dj-public/cta-stop-watch"
 
 
 # delete everything in the folder and then upload the new files
@@ -14,13 +15,13 @@ s3_path = "gs://miurban-dj-public/cta-stop-watch"
 
 
 def store_folder_data(
-    s3_path: str, folder_path: str, s3_location: str, type: str = "cp"
+    bucket_path: str, folder_path: str, bucket_location: str, type: str = "cp"
 ) -> bool:
     """
-    Replace all files in s3_location with files in data_path
+    Replace all files in bucket_location with files in data_path
     """
 
-    command = f"gsutil {type} {folder_path}/* {s3_path}/{s3_location}"
+    command = f"gsutil {type} {folder_path}/* {bucket_path}/{bucket_location}"
     print(command)
     subprocess.run(command, shell=True)
 
@@ -28,13 +29,13 @@ def store_folder_data(
 
 
 def store_file(
-    s3_path: str, file_path: str, s3_location: str, type: str = "cp"
+    bucket_path: str, file_path: str, bucket_location: str, type: str = "cp"
 ) -> bool:
     """
-    Replace all files in s3_location with files in data_path
+    Replace all files in bucket_location with files in data_path
     """
 
-    command = f"gsutil {type} {file_path} {s3_path}/{s3_location}"
+    command = f"gsutil {type} {file_path} {bucket_path}/{bucket_location}"
     subprocess.run(command, shell=True)
 
     return True
@@ -42,48 +43,54 @@ def store_file(
 
 def store_all_data() -> None:
     """
-    push data to s3 bucket, trips by pid, patterns, and timetables
+    push data to google storage bucket, trips by pid, patterns, and timetables
 
     """
     today = str(date.today())
 
-    store_folder_data(s3_path, "data/processed_by_pid", "processed_by_pid/", type="cp")
-    store_folder_data(s3_path, "data/patterns/patterns_raw", "patterns_raw/", type="cp")
-    store_folder_data(s3_path, "data/clean_timetables", "clean_timetables/", type="cp")
+    store_folder_data(
+        BUCKET_PATH, "data/processed_by_pid", "processed_by_pid/", type="cp"
+    )
+    store_folder_data(
+        BUCKET_PATH, "data/patterns/patterns_raw", "patterns_raw/", type="cp"
+    )
+    store_folder_data(
+        BUCKET_PATH, "data/clean_timetables", "clean_timetables/", type="cp"
+    )
 
     store_file(
-        s3_path,
+        BUCKET_PATH,
         f"data/staging/timetables/feed_{today}.zip",
         f"historic_gtfs/feed_{today}.zip",
     )
 
-    # rename current file in s3 bucket to old
+    # rename current file in google storage bucket to old
     store_file(
-        s3_path,
-        f"{s3_path}/metrics/stop_metrics_df_latest.parquet",
+        BUCKET_PATH,
+        f"{BUCKET_PATH}/metrics/stop_metrics_df_latest.parquet",
         "metrics/stop_metrics_df_previous.parquet",
         type="mv",
     )
 
     # add latest file
     store_file(
-        s3_path,
+        BUCKET_PATH,
         "data/metrics/stop_metrics_df.parquet",
         "metrics/stop_metrics_df_latest.parquet",
         type="cp",
     )
 
-    # rename current file in s3 bucket to old
+    # rename current file in google storage bucket to old
     store_file(
-        s3_path,
-        f"{s3_path}/metrics/stop_metrics_df_latest.csv",
+        BUCKET_PATH,
+        f"{BUCKET_PATH}/metrics/stop_metrics_df_latest.csv",
         "metrics/stop_metrics_df_previous.csv",
         type="mv",
     )
 
     # add latest file
     store_file(
-        s3_path,
+        BUCKET_PATH,
         "data/metrics/stop_metrics_df.csv",
         "metrics/stop_metrics_df_latest.csv",
         type="cp",
